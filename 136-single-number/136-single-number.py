b92cc1309bb52a1a29e05df489d794898bfbb6cc
AMX0013 <42344377+AMX0013@users.noreply.github.com>
2022-03-10 19:50:46 +0530

    Time: 300 ms (14.88%), Space: 16.9 MB (10.52%) - LeetHub

---
+++ 136-single-number/136-single-number.py
@@ -0,0 +1,16 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        hashMap = {}
+        for element in nums:
+            hashMap[element]=0
+        for element in nums:
+            hashMap[element] += 1
+        print(hashMap)    
+        output = 0
+        
+        for element in hashMap:
+            print(element, hashMap[element])
+            if hashMap[element] == 1:
+                output = element
+        return output
+        
