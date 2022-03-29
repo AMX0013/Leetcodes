a31e15ee28d3260d84b90b5164e81913bb00221f
AMX0013 <42344377+AMX0013@users.noreply.github.com>
2022-03-29 18:14:50 +0530

    Time: 96 ms (41.23%), Space: 15.2 MB (71.88%) - LeetHub

--- 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.py
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.py
@@ -1,25 +1,6 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        L = 0
-        R=0
-        rows = max(prices)+1
-        profit = 0
         profit2 = 0
-        #memo =[[0]*rows]*rows
-        while R < len(prices):
-            
-            if prices[R] < prices[L]:
-                L= R
-                
-                
-            if prices[L] < prices[R]:
-                                             
-                res = prices[R] - prices[L]
-
-                if profit < res:
-                    profit = res
-            
-            R+=1
         L=0
         R=1
         while L < len(prices) and R < len(prices):
@@ -41,5 +22,5 @@ class Solution:
 
         
         
-        return max(profit,profit2)
+        return profit2
         
