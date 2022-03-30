ced59cff523fc3cc3657821d362342fa30cdd7f9
AMX0013 <42344377+AMX0013@users.noreply.github.com>
2022-03-30 20:35:26 +0530

    Time: 1961 ms (12.80%), Space: 78.4 MB (17.74%) - LeetHub

---
+++ 714-best-time-to-buy-and-sell-stock-with-transaction-fee/714-best-time-to-buy-and-sell-stock-with-transaction-fee.py
@@ -0,0 +1,36 @@
+class Solution:
+    
+    def solve(self, own ,index,prices, memo,fee) -> int:
+        default = 0
+        
+        if index == len(prices) :
+            return default
+        #print( index ,own , prices[index],">",memo)
+        if memo[index][own] != None:
+            return memo[index][own]
+        if own == 1:    # if a stock is  owned on day i > find max(selling that stock and call it again or hold the stock and call func again for next day)
+            
+            profit1 = prices[index] + self.solve(0,index+1,prices,memo,fee) #sell
+            profit2 = self.solve(1,index+1,prices,memo,fee) #hold
+            memo[index][own] = max(profit1,profit2) 
+            #print("own ==1","profit = ",max(profit1,profit2),profit1,profit2 ,index ,own , prices[index],">",memo)
+            return memo[index][own]
+            
+            
+        else: # if a stock is not owned on day i > find max(buying that stock and call it again or dont buy and call func again for next day)
+            
+            profit1 = -(prices[index]+fee) + self.solve(1,index+1,prices,memo,fee) #buy
+            profit2 = self.solve(0,index+1,prices,memo,fee) #skip
+            memo[index][own] = max(profit1,profit2) 
+            #print("own ==0","profit = ",max(profit1,profit2),profit1,profit2 ,index ,own , prices[index],">",memo)
+            return memo[index][own]
+    
+    def maxProfit(self, prices: List[int], fee: int) -> int:
+        cols = 2
+        rows = len(prices)
+        memo = [[None]*cols for i in range(rows)]
+        #print(memo)
+        x=self.solve(0,0,prices,memo,fee)
+        #print("Final:",memo)
+        return x
+        
