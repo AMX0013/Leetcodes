c8981e86701b92eb7fe055fd7949f2e7abf67f03
AMX0013 <42344377+AMX0013@users.noreply.github.com>
2022-03-31 19:32:12 +0530

    Time: 1428 ms (5.03%), Space: 26.9 MB (29.48%) - LeetHub

---
+++ 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.py
@@ -0,0 +1,39 @@
+class Solution:
+    def solve(self, own ,index,prices, memo,count) -> int:
+        default = 0
+        
+        if index == len(prices) :
+            return default
+        if count<=0:
+            return default
+        #print( index ,own , prices[index],">",memo)
+        print(index,own,count)
+        if memo[count][index][own] != None:
+            return memo[count][index][own]
+        if own == 1:    # if a stock is  owned on day i > find max(selling that stock and call it again or hold the stock and call func again for next day)
+            
+            profit1 = prices[index] + self.solve(0,index+1,prices,memo,count-1) #sell
+            profit2 = self.solve(1,index+1,prices,memo,count) #hold
+            memo[count][index][own] = max(profit1,profit2) 
+            #print("own ==1","profit = ",max(profit1,profit2),profit1,profit2 ,index ,own , prices[index],">",memo)
+            return memo[count][index][own]
+            
+            
+        else: # if a stock is not owned on day i > find max(buying that stock and call it again or dont buy and call func again for next day)
+            
+            profit1 = -(prices[index]) + self.solve(1,index+1,prices,memo,count) #buy
+            profit2 = self.solve(0,index+1,prices,memo,count) #skip
+            memo[count][index][own] = max(profit1,profit2) 
+            #print("own ==0","profit = ",max(profit1,profit2),profit1,profit2 ,index ,own , prices[index],">",memo)
+            return memo[count][index][own]
+    def maxProfit(self, k: int, prices: List[int]) -> int:
+        a = 2
+        b = len(prices)
+        c=k+1
+        memo = [[ [None for col in range(a)] for col in range(b)] for row in range(c)] # memo = [[None]*cols]*rows <this broke the memo : 
+        #print(memo[1][0][0])
+        #print(memo)
+        x=self.solve(0,0,prices,memo,k)
+        #print("Final:",memo)
+        return x
+        
