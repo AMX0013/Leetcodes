0dfa476c708ff5190fbcdf9ec86b3f14339489c4
AMX0013 <42344377+AMX0013@users.noreply.github.com>
2022-05-05 21:18:55 +0530

    Time: 1074 ms (5.29%), Space: 84.4 MB (5.29%) - LeetHub

--- 77-combinations/77-combinations.py
+++ 77-combinations/77-combinations.py
@@ -2,13 +2,13 @@ class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
         result = []
         
-        
+        #base case: recursion reaches here first
         if k ==1:
             for i in range(1,n+1):
-                result.append([i])
+                result.append([i])  # returns [1,...,n]
             return result
         
-        else:
+        else: # will create +1 digit combinations of returned result
             #print("k level =",k)
             result = self.combine(n,k-1)
             #print(result)
@@ -19,8 +19,8 @@ class Solution:
                 #print("#subcombo of Result :",result[y])
                 #print("atlest = ", atleast)
                 for i in range(atleast+1,n+1):
-                    
-                    x = result[y][:] # idek how but using [:] sends a copy instead of the whole shit
+                    # will create +1 digit combinations of returned result
+                    x = result[y][:] # idek how but using [:] sends a copy instead of the whole shit , preventing result[y] from being modified
                     #print("before",x)
                     x.append(i)
                     #print("after",x)
