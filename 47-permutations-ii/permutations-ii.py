13ab21e6346f0bbba21b08befb624f621f13e927
Amudheswaran Sankaranarayanan <42344377+AMX0013@users.noreply.github.com>
2023-08-02 04:50:55 -0400

    Time: 96 ms (30.20%) | Memory: 17.6 MB (6.93%) - LeetSync

--- 47-permutations-ii/permutations-ii.py
+++ 47-permutations-ii/permutations-ii.py
@@ -1,30 +1,77 @@
+# class Solution:
+#     def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+
+#         if len(nums)==1:
+#             return [nums]
+        
+#         memo = {}
+
+#         def recur(splice):
+#             res = []
+            
+
+#             if str(sorted(splice)) in memo:
+#                 return memo[str(sorted(splice))]
+
+            
+#             if len(splice) == 1:
+#                 memo[str(splice)] = [splice[:]]
+#                 return [splice[:]]
+
+#             for _ in range(len(splice)):
+                
+#                 head = splice.pop(0)
+
+#                 perms = recur(splice)
+#                 # print(splice,perms)
+#                 for perm in perms:
+#                     perm.append(head)
+#                     if perm not in res:
+#                         res.append(perm)
+#                 splice.append(head)
+
+#             memo[str(sorted(splice))] = res               
+
+#             return res
+#         res = recur(nums)
+
+#         for key , value in memo.items():
+#             print(key," : ",value)
+        
+
+#         return res
+
+
+
 class Solution:
     def permuteUnique(self, nums: List[int]) -> List[List[int]]:
 
         if len(nums)==1:
             return [nums]
         
-        def recur(splice):
-            res = []
+        memo = {}
 
+        def recur(splice: List[int]):
             if len(splice) == 1:
                 return [splice[:]]
 
-            for _ in range(len(splice)):
+            key = tuple(sorted(splice))
+            if key in memo:
+                return memo[key]
 
-                head = splice.pop(0)
-
-                perms = recur(splice)
-                print(splice,perms)
+            res = []
+            for i in range(len(splice)):
+                splice_copy = splice[:]
+                head = splice_copy.pop(i)
+                perms = recur(splice_copy)
                 for perm in perms:
-                    perm.append(head)
+                    perm= [head] + perm
                     if perm not in res:
                         res.append(perm)
 
-                splice.append(head)
-
+            memo[key] = res
             return res
-
-        # recur(nums)
-
-        return recur(nums)
+        res = recur(nums)
+        for key , value in memo.items():
+            print(key," : ",value)
+        return res
