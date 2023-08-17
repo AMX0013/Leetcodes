dc873548111d1d299661b7a09755baa6276528e7
Amudheswaran Sankaranarayanan <42344377+AMX0013@users.noreply.github.com>
2023-08-17 05:17:15 -0400

    Time: 729 ms (31.35%) | Memory: 20.1 MB (37.02%) - LeetSync

--- 542-01-matrix/01-matrix.py
+++ 542-01-matrix/01-matrix.py
@@ -5,16 +5,19 @@ class Solution:
         width = len(mat[0])
 
         bfsQ = collections.deque()
+        INF = 10**10
+
+        res = [[INF for _ in range(width)] for _ in range(height)]
 
         for h in range(height):
             for w in range(width):
                 if mat[h][w]==0:
+                    res[h][w] = 0
                     bfsQ.append((0,h,w))
 
-        res = [[math.inf for _ in range(width)] for _ in range(height)]
 
         directions = [(1,0),(0,1),(-1,0),(0,-1)]
-        visited = [[False for _ in range(width)] for _ in range(height)]
+        # visited = [[False for _ in range(width)] for _ in range(height)]
 
         while bfsQ:
             # popleft from Queue
@@ -22,14 +25,16 @@ class Solution:
 
             # populate distance 
             res[h][w] = min(res[h][w],dist)
-            visited[h][w] = True
+            # visited[h][w] = True
 
             for dir_h,dir_w in directions:
 
                 dh = h+dir_h
                 dw = w+dir_w
 
-                if 0<=dh<height and 0<=dw<width and visited[dh][dw] == False:
+                # if 0<=dh<height and 0<=dw<width and visited[dh][dw] == False:
+                if 0<=dh<height and 0<=dw<width and res[dh][dw] == INF:
+
                     bfsQ.append((dist+1,dh,dw))
 
         return res
