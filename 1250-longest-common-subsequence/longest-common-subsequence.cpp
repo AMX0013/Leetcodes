e69edfe130e5e393ebd16f4dbe1210ff273ae150
Amudheswaran Sankaranarayanan <42344377+AMX0013@users.noreply.github.com>
2024-01-23 02:21:31 -0500

    Time: 21 ms (90.47%) | Memory: 7.7 MB (87.68%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -1,19 +1,20 @@
 class Solution {
 public:
     int longestCommonSubsequence(string text1, string text2) {
-        vector<vector<short>> dp(text2.size()+1, vector<short>(text1.size()+1, 0));
+        vector<vector<short>> dp(2, vector<short>(text1.size()+1, 0));
 
         for( short i = 0; i<text2.size();i++){
             for (short j = 0; j<text1.size(); j++){
                 // if the chars match
                 if (text2[i] == text1[j]){
-                    dp[i+1][j+1] = dp[i][j]+1;
+                    dp[1][j+1] = dp[0][j]+1;
                 }
                 else{
-                     dp[i+1][j+1] = max(dp[i+1][j] , dp[i][j+1]);
+                     dp[1][j+1] = max(dp[1][j] , dp[0][j+1]);
                 }
             }
+            dp[0] = dp[1];
         }
-        return dp[text2.size()][text1.size()];
+        return dp[1][text1.size()];
     }
 };
