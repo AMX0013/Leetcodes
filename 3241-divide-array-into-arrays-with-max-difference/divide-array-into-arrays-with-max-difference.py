cc7122ff53e082aa5995dcce923631223ee46529
Amudheswaran Sankaranarayanan <42344377+AMX0013@users.noreply.github.com>
2024-02-01 20:46:51 -0500

    Time: 1256 ms (5.05%) | Memory: 35.6 MB (5.18%) - LeetSync

--- 3241-divide-array-into-arrays-with-max-difference/divide-array-into-arrays-with-max-difference.py
+++ 3241-divide-array-into-arrays-with-max-difference/divide-array-into-arrays-with-max-difference.py
@@ -1,21 +1,47 @@
 class Solution:
     def divideArray(self, nums: List[int], k: int) -> List[List[int]]:
-        size = len(nums)
+        size = len(nums)       
+
+        dp = {}
+
+        for num in nums:
+            dp[num] = dp.get(num,0)+1
+
+        print(dp)
         
-
-        nums.sort()
-
         result = []
-        # group_index = 0
-        for i in range(0, size, 3):
-            if i + 2 < size and nums[i + 2] - nums[i] <= k:
-                result.append([nums[i], nums[i + 1], nums[i + 2]])
-                # group_index += 1
-            else:
-                return []
+
+        subRes = [] 
+
+        for i in range(min(nums) , max(nums)+1):
+            if i in dp.keys():
+                while dp[i] >0:
+                    val = i
+                    dp[i] -=1
+                    print("Current subRes:", subRes, "val = ", val)
+                    match len(subRes):
+                        
+                        case 2:
+                            if val - subRes[0] <=k:
+                                subRes.append(val)
+                                result.append(subRes)
+                                subRes = []
+                            else:
+                                return []
+                                
+                        case 1:
+                            if val - subRes[0] <=k:
+                                subRes.append(val)
+                            else:
+                                return []
+                        case 0:
+                            subRes.append(val)
+                        case _:
+                            return []
+                            
+                
         return result
 
-        
 
             
 
