0cf430e5eca71b584342a515aa3ab299978393e3
Amudheswaran Sankaranarayanan <42344377+AMX0013@users.noreply.github.com>
2024-02-01 22:13:19 -0500

    Time: 30 ms (87.58%) | Memory: 16.8 MB (51.55%) - LeetSync

--- 1212-sequential-digits/sequential-digits.py
+++ 1212-sequential-digits/sequential-digits.py
@@ -1,25 +1,18 @@
 class Solution:
 
-    def digit_arr(self,num: int) -> int:
-        # digit_arr = []
+    def getNumLen(self,num: int) -> int:
+        
         digits = 0
-
         while num > 0:
             print(num%10)
-            # mod = num%10
-            # digit_arr.append(mod)
             digits+=1
             num = num//10
 
-        # digit_arr.reverse()
-
-        # return digit_arr
         return digits
 
     def constructLowest(self, digits:int, low:int ) -> int:
         
         low_arr = []
-        # print("length of low =", len(str(low)), 10**( len(str(low))  -1  ) )
         first_digit = low//(10**( digits -1 ) )
         print(low, "first_digit", first_digit)
 
@@ -32,8 +25,8 @@ class Solution:
         return number
 
     def sequentialDigits(self, low: int, high: int) -> List[int]:
-        low_size = self.digit_arr(low)
-        high_size = self.digit_arr(high)
+        low_size = self.getNumLen(low)
+        high_size = self.getNumLen(high)
 
         print(low_size , high_size)
         res = []
@@ -47,11 +40,13 @@ class Solution:
             ones_arr = [1]*size
             one =int(''.join(str(element) for element in (ones_arr) ))
             
+            if num < low:
+                num = num + one
+
             print("ONE:",one)
             while num %10 != 0 and num <= high  :
                 print(num)
-                if num >= low:
-                    res.append(num)
+                res.append(num)
                 num = num+one
 
 
