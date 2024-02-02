32fb8ffe8160dc121e078286f2b106d32e082879
Amudheswaran Sankaranarayanan <42344377+AMX0013@users.noreply.github.com>
2024-02-02 04:38:06 -0500

    Time: 56 ms (57.46%) | Memory: 16.6 MB (64.52%) - LeetSync

--- 39-combination-sum/combination-sum.py
+++ 39-combination-sum/combination-sum.py
@@ -2,25 +2,25 @@ class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         memo = {}
         res = []
-        def recurSum(index,splice,total):
+        def recurSum(index,combo_arr,total):
             
             if total == target:
-                if splice not in res:
-                    res.append(splice.copy())
+                if combo_arr not in res:
+                    res.append(combo_arr.copy())
                     return
             if total > target or index >=len(candidates):
                 return
             
             # 1st call considering the element too
-            splice.append(candidates[index])
-            recurSum(index , splice, total+candidates[index])
+            combo_arr.append(candidates[index])
+            recurSum(index , combo_arr, total+candidates[index])
 
             # pop from curr, before next one
 
-            splice.pop()
+            combo_arr.pop()
 
             #2nd call without having the element
-            recurSum(index+1 , splice, total)
+            recurSum(index+1 , combo_arr, total)
 
 
         recurSum(0,[],0)
