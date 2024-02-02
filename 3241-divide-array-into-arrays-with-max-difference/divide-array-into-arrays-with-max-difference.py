b3e9f0e0b536fdc67777c1067e025833d972c402
Amudheswaran Sankaranarayanan <42344377+AMX0013@users.noreply.github.com>
2024-02-01 20:59:13 -0500

    Time: 689 ms (98.47%) | Memory: 30.7 MB (98.10%) - LeetSync

--- 3241-divide-array-into-arrays-with-max-difference/divide-array-into-arrays-with-max-difference.py
+++ 3241-divide-array-into-arrays-with-max-difference/divide-array-into-arrays-with-max-difference.py
@@ -23,7 +23,7 @@ class Solution:
         
 
         for i in range(0,len(nums)-2,3):
-            if nums[i+2]-nums[i] <= k and  nums[i+1]-nums[i] <= k:
+            if nums[i+2]-nums[i] <= k :
                 result.append( nums[i:i+3] )
             else:
                 return []
