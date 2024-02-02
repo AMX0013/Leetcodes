2d94db40257a7404449a0e3a967e5a4c0239164d
Amudheswaran Sankaranarayanan <42344377+AMX0013@users.noreply.github.com>
2024-02-01 20:58:19 -0500

    Time: 711 ms (92.48%) | Memory: 30.7 MB (96.19%) - LeetSync

--- 3241-divide-array-into-arrays-with-max-difference/divide-array-into-arrays-with-max-difference.py
+++ 3241-divide-array-into-arrays-with-max-difference/divide-array-into-arrays-with-max-difference.py
@@ -1,22 +1,23 @@
 class Solution:
     def divideArray(self, nums: List[int], k: int) -> List[List[int]]:
-        dp = {}
+        # dp = {}
 
-        for num in nums:
-            dp[num] = dp.get(num,0)+1
+        # for num in nums:
+        #     dp[num] = dp.get(num,0)+1
 
-        min_nums, max_nums = min(nums) , max(nums)
-        # re-empty
-        nums = []
+        # min_nums, max_nums = min(nums) , max(nums)
+        # # re-empty
+        # nums = []
          
-        # Count Sort for O(N)
-        for i in range( min_nums, max_nums+1):
-            if i in dp.keys():
-                while dp[i] >0:
-                    nums.append(i)
-                    dp[i] -=1
-        dp = None
+        # # Count Sort for O(N)
+        # for i in range( min_nums, max_nums+1):
+        #     if i in dp.keys():
+        #         while dp[i] >0:
+        #             nums.append(i)
+        #             dp[i] -=1
+        # dp = None
 
+        nums.sort()
         # Actual logic
         result = []
         
