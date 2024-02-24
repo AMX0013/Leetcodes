5c6106ccfdbb1a6241a4e333f4fbf631c653d113
Amudheswaran Sankaranarayanan <42344377+AMX0013@users.noreply.github.com>
2024-02-23 21:25:14 -0500

    Time: 125 ms (46.97%) | Memory: 18.3 MB (50.75%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.py
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.py
@@ -1,7 +1,6 @@
 class Solution:
     def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
 
-
         adj={i:[] for i in range(n+1)}
         for u,v,val in flights:
             adj[u].append((v,val))
@@ -13,17 +12,17 @@ class Solution:
         hops  = 0
         
         visited = [10**9]*n
-        path = [src]
-        print(path)
-        heapq.heappush(heapQ,(price,hops,src,path))
+        # path = [src]
+        # print(path)
+        heapq.heappush(heapQ,(price,hops,src))
         
         resPrice = -1
 
         while heapQ:
 
-            price, hops,curr,path = heapq.heappop(heapQ)
+            price, hops,curr = heapq.heappop(heapQ)
             
-            print("price, hops,curr",price, hops,curr,path)
+            print("price, hops,curr",price, hops,curr)
 
             if curr == dst:
                 print("cheapest is ",price)
@@ -36,15 +35,15 @@ class Solution:
             # visited tracks if i have come to a node with said hops
             # if i come there again with more hops, its a longer path and can be pruned
             if hops >= visited[curr]  :
-                print("visited ex again. Ex is :",curr, path)
+                print("visited ex again. Ex is :",curr)
                 print(visited)
                 continue
             visited[curr] = hops
 
             for next_dest, next_price in adj[curr]:
-                temp = path.copy()
-                temp.append(next_dest)
-                heapq.heappush(heapQ,(price + next_price ,hops +1 ,next_dest,temp ))
+                # temp = path.copy()
+                # temp.append(next_dest)
+                heapq.heappush(heapQ,(price + next_price ,hops +1 ,next_dest ))
         
         return resPrice
 
