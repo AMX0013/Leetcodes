ab16c172cee2ee1260d06177ae131af44a842ecf
Amudheswaran Sankaranarayanan <42344377+AMX0013@users.noreply.github.com>
2024-03-25 19:17:09 -0400

    Time: 977 ms (44.95%) | Memory: 11.9 MB (19.05%) - LeetSync

--- 79-word-search/word-search.cpp
+++ 79-word-search/word-search.cpp
@@ -11,7 +11,7 @@ public:
     // optimization: prevent usage of visited, memory space by temporarily changing the curr loc var
     bool wordFind( vector<vector<char>>& board, string word, int row,int col,int height,int width  ,int strIdx){
     
-        if ( (0<=row && row < board.size() ) and ( 0<=col && col <board[row].size() ) ) {
+        if ( -1<row && -1<col && row < height  && col <width  ) {
         
             if (word[strIdx] == board[row][col] ) {
                 strIdx+=1;
@@ -19,7 +19,7 @@ public:
             else{
                 return false;
             }
-            if (strIdx == word.length() ) {
+            if (strIdx == word.size() ) {
                     // finally reached!
                     return true;
             }
