5bc421fa4fa2b1b6bf557bfd88db4b8a42abfc1c
Amudheswaran Sankaranarayanan <42344377+AMX0013@users.noreply.github.com>
2024-04-22 02:12:59 -0400

    Time: 911 ms (92.67%) | Memory: 32.2 MB (89.47%) - LeetSync

--- 2085-array-with-elements-not-equal-to-average-of-neighbors/array-with-elements-not-equal-to-average-of-neighbors.py
+++ 2085-array-with-elements-not-equal-to-average-of-neighbors/array-with-elements-not-equal-to-average-of-neighbors.py
@@ -1,17 +1,23 @@
 class Solution:
     def rearrangeArray(self, nums: List[int]) -> List[int]:
-        heapq.heapify(nums)
-        n = len(nums)
-        res = [0]*n
-        i = 0
-        while i < n:
-            res[i] = heapq.heappop(nums)
-            i+=2
-        i = 1
-        while i < n:
-            res[i] = heapq.heappop(nums)
-            i+=2
 
-        return res
+        for i in range(1,len(nums)-1):
+            if nums[i-1] < nums[i] < nums[i+1] or  nums[i-1] > nums[i] > nums[i+1]:
+                nums[i] , nums[i+1] = nums[i+1], nums[i]
+        return nums
+
+        # heapq.heapify(nums)
+        # n = len(nums)
+        # res = [0]*n
+        # i = 0
+        # while i < n:
+        #     res[i] = heapq.heappop(nums)
+        #     i+=2
+        # i = 1
+        # while i < n:
+        #     res[i] = heapq.heappop(nums)
+        #     i+=2
+
+        # return res
 
         
