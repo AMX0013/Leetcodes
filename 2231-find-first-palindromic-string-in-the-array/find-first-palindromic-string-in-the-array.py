db3e221e9cf703fd9f48d7e6c771891e7ac96ef9
Amudheswaran Sankaranarayanan <42344377+AMX0013@users.noreply.github.com>
2024-04-28 00:00:01 -0400

    Time: 74 ms (68.74%) | Memory: 16.7 MB (21.04%) - LeetSync

--- 2231-find-first-palindromic-string-in-the-array/find-first-palindromic-string-in-the-array.py
+++ 2231-find-first-palindromic-string-in-the-array/find-first-palindromic-string-in-the-array.py
@@ -1,9 +1,9 @@
 class Solution:
-    def isPali(self,word):
-        return word == word[::-1]
+    # def isPali(self,word):
+    #     return word == word[::-1]
     def firstPalindrome(self, words: List[str]) -> str:
         for word in words:
-            if self.isPali(word):
+            if word == word[::-1]:
                 return word
         return ""
 
