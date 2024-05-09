a232ec22c805900856b3aeb01631528acff3a049
Amudheswaran Sankaranarayanan <42344377+AMX0013@users.noreply.github.com>
2024-05-08 20:50:49 -0400

    Time: 530 ms (41.11%) | Memory: 17.7 MB (21.46%) - LeetSync

--- 3395-minimum-length-of-anagram-concatenation/minimum-length-of-anagram-concatenation.py
+++ 3395-minimum-length-of-anagram-concatenation/minimum-length-of-anagram-concatenation.py
@@ -4,21 +4,24 @@ class Solution:
     def minAnagramLength(self, s: str) -> int:
         n = len(s)
         res = len(s)
+
+        def check(seg_len):
+            anagram = sorted(s[:seg_len])
+            # print("Testing for the anagram",str(anagram))
+            
+            for i in range(seg_len,n,seg_len):
+                word =  sorted(s[i:i+seg_len])
+                if anagram != word:
+                    # print("failed for: ", str(word))
+                    return False                    
+            return True
+
         for seg_len in range(1,n//2 +1 ):
             if n%seg_len != 0:
                 continue
-            anagram = Counter(s[:seg_len])
-            # print("Testing for the anagram",str(anagram))
-            is_anagram = True
-            # else
-            for i in range(seg_len,n,seg_len):
-                word =  Counter(s[i:i+seg_len])
-                if anagram != word:
-                    # print("failed for: ", str(word))
-                    is_anagram = False
-                    break
-            if is_anagram:
+            if check(seg_len):
                 return seg_len
+            
         # s is the min anagram
         return res
         
