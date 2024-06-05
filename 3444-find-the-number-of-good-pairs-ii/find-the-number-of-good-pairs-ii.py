fdb490fcd6486041aca1fe9cb43bd3027061bcb8
Amudheswaran Sankaranarayanan <42344377+AMX0013@users.noreply.github.com>
2024-06-05 14:45:04 -0400

    Time: 1298 ms (76.78%) | Memory: 135.2 MB (5.05%) - LeetSync

--- 3444-find-the-number-of-good-pairs-ii/find-the-number-of-good-pairs-ii.py
+++ 3444-find-the-number-of-good-pairs-ii/find-the-number-of-good-pairs-ii.py
@@ -2,19 +2,21 @@ class Solution:
     def numberOfPairs(self, nums1: List[int], nums2: List[int], k: int) -> int:
         
         countNums1 = Counter(nums1)  
+        countNums2 = Counter(nums2) 
         limit = max(nums1) 
         multiplesCount = {}
 
-        if k == 1 and nums2 == [1]*len(nums2):
-            return len(nums2) * len(nums1)       
 
-        for num2 in nums2:
+        # if k == 1 and nums2 == [1]*len(nums2):
+        #     return len(nums2) * len(nums1)       
+
+        for num2 , count2 in countNums2.items():
             multiple = num2 * k
             for multiple_value in range(multiple, limit + 1, multiple):
                 if multiple_value in multiplesCount:
-                    multiplesCount[multiple_value] += 1
+                    multiplesCount[multiple_value] += count2
                 else:
-                    multiplesCount[multiple_value] = 1
+                    multiplesCount[multiple_value] = count2
         
         res = 0
         for num1 in countNums1.keys() :
