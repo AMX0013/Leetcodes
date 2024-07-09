df2976071cc4a71807af4cd74bc22db1c9f4e53d
Amudheswaran Sankaranarayanan <42344377+AMX0013@users.noreply.github.com>
2024-07-09 00:44:55 -0700

    Time: 788 ms (15.12%) | Memory: 57.3 MB (9.97%) - LeetSync

--- 1803-average-waiting-time/average-waiting-time.py
+++ 1803-average-waiting-time/average-waiting-time.py
@@ -4,13 +4,13 @@ class Solution:
         # chef_ready_time = 0
         total_wait = 0
         for customer in customers:
-            print("Chef ready at", chef_ready_time)
+            # print("Chef ready at", chef_ready_time)
             # print(customer)
             arrival, dur = customer[0], customer[1]
 
             
             after_start_delay = max(chef_ready_time - arrival,0 ) + dur 
-            print("delay for customer ", customer, "is:"  , after_start_delay)
+            # print("delay for customer ", customer, "is:"  , after_start_delay)
 
             chef_ready_time = max(chef_ready_time, arrival) + dur
             # total_wait += before_start_delay + after_start_delay
