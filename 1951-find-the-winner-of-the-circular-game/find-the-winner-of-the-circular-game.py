b8c05b44de909b51ce7c9a86f4cf9ecafbcf201a
Amudheswaran Sankaranarayanan <42344377+AMX0013@users.noreply.github.com>
2024-07-09 00:05:18 -0700

    Time: 42 ms (47.62%) | Memory: 16.5 MB (49.16%) - LeetSync

--- 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.py
+++ 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.py
@@ -5,7 +5,9 @@ class Solution:
         # start = 0
         # q = deque([x for x in range(1,n+1)])
         # # print(q)
-
+        '''
+        1: A deque to rotate the list keeping our pointer stationary
+        '''
         # for siz in range(n,1,-1):
 
         #     for _ in range(k):
@@ -16,7 +18,10 @@ class Solution:
 
         
         # return q[0]
-
+        '''
+        2:, based on neetcode's explanation, we expland deque solution with prints
+        and track how the position of our result varied. with values of n for given k
+        '''
         def solver(n,k):
             if n ==1:
                 return 0
@@ -24,5 +29,15 @@ class Solution:
             return (solver(n-1,k) + k) % n
         return solver(n,k)+1
         
+        '''
+        3> Unwrap 2, with a bottom up only. 
+        '''
+        res = 0
+
+        for curr_size in range(n+1,1,-1):
+            res += (res+k) % curr_size
+
+        return res+1
+
 
         
