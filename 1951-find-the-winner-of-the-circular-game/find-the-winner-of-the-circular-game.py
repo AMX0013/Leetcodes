5cfa90eecd4ffc8b84efd90e2f680597474a80e0
Amudheswaran Sankaranarayanan <42344377+AMX0013@users.noreply.github.com>
2024-07-08 23:57:38 -0700

    Time: 41 ms (51.39%) | Memory: 16.5 MB (49.16%) - LeetSync

--- 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.py
+++ 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.py
@@ -2,19 +2,27 @@ from collections import deque
 
 class Solution:    
     def findTheWinner(self, n: int, k: int) -> int:
-        start = 0
-        q = deque([x for x in range(1,n+1)])
-        # print(q)
+        # start = 0
+        # q = deque([x for x in range(1,n+1)])
+        # # print(q)
 
-        for siz in range(n,1,-1):
+        # for siz in range(n,1,-1):
 
-            for _ in range(k):
-                temp = q.popleft()
-                q.append(temp)
-            q.pop()
-            # print(q)
+        #     for _ in range(k):
+        #         temp = q.popleft()
+        #         q.append(temp)
+        #     q.pop()
+        #     # print(q)
 
         
-        return q[0]
+        # return q[0]
+
+        def solver(n,k):
+            if n ==1:
+                return 0
+            # res is at loc (prev_res +k) % curr_size: n
+            return (solver(n-1,k) + k) % n
+        return solver(n,k)+1
+        
 
         
