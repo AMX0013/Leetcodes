30778089d96f2d5ae5cd623700d6f297c533d2a5
Amudhesh <42344377+AMX0013@users.noreply.github.com>
2024-08-19 14:47:27 -0400

    Time: 42 ms (18.10%) | Memory: 16.6 MB (46.48%) - LeetSync

--- 1585-the-kth-factor-of-n/the-kth-factor-of-n.py
+++ 1585-the-kth-factor-of-n/the-kth-factor-of-n.py
@@ -1,25 +1,16 @@
 class Solution:
     def kthFactor(self, n: int, k: int) -> int:
         # Find all factors of n
-        res = set()
+        count = 0
         # all factors should be from 1 to sqrt(n)
-        for i in range(1,int(sqrt(n))+1 ):
+        for i in range(1,n+1):
             if n%i==0:
-                res.add(i)
-                res.add( n//i )
-        # print(res)
-        num = -1
-        if len(res) < k:
-            return num
-        # in a sorted list of all factors of n,
-        res = list(res)
-        heapify(res)
-        # print(res)
-        
-        
-        for _ in range(k):
-            num = heapq.heappop(res)
+                count +=1
+                # res = i
+                if count == k:
+                    return i
+                # res.add( n//i )
+        return -1
 
-        return num
-        # return the kth index elemnt
+        
         
