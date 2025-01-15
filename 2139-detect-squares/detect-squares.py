b2a7edb12a84fcc8c46b001b0cce59c51a5f49c9
Amudhesh <42344377+AMX0013@users.noreply.github.com>
2025-01-15 02:11:30 -0600

    Time: 107 ms (52.96%) | Memory: 19.5 MB (40.65%) - LeetSync

---
+++ 2139-detect-squares/detect-squares.py
@@ -0,0 +1,45 @@
+class DetectSquares:
+
+    def __init__(self):
+        self.grid = {}
+        
+
+    def add(self, point: List[int]) -> None:
+        self.grid[(point[0], point[1])] = self.grid.get( (point[0], point[1]), 0 ) + 1
+        #  Duplicate points are allowed and should be treated as different points.
+        return
+
+    def count(self, p: List[int]) -> int:
+        count = 0
+        for d in self.grid.keys():
+            # find one p is valid, use that and check if dictionary has other 2
+            # since square, donal is critical
+            
+            if abs(d[0] - p[0]) == abs(d[1] - p[1]) and abs(d[1] - p[1]) >0 :
+                dxpy    = (d[0], p[1])
+                pxdy    = (p[0], d[1])
+                point   = (p[0], p[1])
+                diag    = (d[0], d[1])
+                
+                if dxpy in self.grid and pxdy in self.grid:
+                    num_sqrs = self.grid.get(diag,0)  * self.grid.get(dxpy) * self.grid.get(pxdy)
+
+                    # print(num_sqrs)
+                    # if point in self.grid:
+                    #     num_sqrs*=self.grid.get(point)
+                        # print(num_sqrs)
+                    # print('dxpy',dxpy,self.grid.get(dxpy))
+                    # print('pxdy',pxdy,self.grid.get(pxdy))
+                    # print('point',point,self.grid.get(point))
+                    # print('diag',diag,self.grid.get(diag))
+                    count+=num_sqrs
+        
+        # print('Ans: ',count)
+        # print()
+        return count
+
+
+# Your DetectSquares object will be instantiated and called as such:
+# obj = DetectSquares()
+# obj.add(point)
+# param_2 = obj.count(point)
