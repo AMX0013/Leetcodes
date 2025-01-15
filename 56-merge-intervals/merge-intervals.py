4203d99beee5025e4bbb28ff303cdb7d4fea4d3b
Amudhesh <42344377+AMX0013@users.noreply.github.com>
2025-01-15 00:41:51 -0600

    Time: 19 ms (6.14%) | Memory: 21 MB (42.22%) - LeetSync

---
+++ 56-merge-intervals/merge-intervals.py
@@ -0,0 +1,19 @@
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        intervals = sorted(intervals, key= lambda x: x[0], reverse = False)
+        # print(intervals)
+        i=0
+        while(i < len(intervals)-1):
+            if intervals[i][1] >= intervals[i+1][0] :
+                r = max(intervals[i][1], intervals[i+1][1])
+                # merge the two intervals 
+                mergedInterval = [intervals[i][0], r]
+                # and store
+                intervals.pop(i)
+                intervals[i] = mergedInterval
+                # print('Intervals after merging:',intervals)
+            else:
+                i+=1
+            # print(i)
+        return intervals
+        
