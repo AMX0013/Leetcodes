222282b486953ae2b80ebbe180d3b21883c494cb
Amudhesh <42344377+AMX0013@users.noreply.github.com>
2025-01-25 18:17:19 -0600

    Time: 250 ms (91.72%) | Memory: 95.2 MB (5.10%) - LeetSync

--- 3219-make-lexicographically-smallest-array-by-swapping-elements/make-lexicographically-smallest-array-by-swapping-elements.py
+++ 3219-make-lexicographically-smallest-array-by-swapping-elements/make-lexicographically-smallest-array-by-swapping-elements.py
@@ -7,7 +7,7 @@ class Solution:
         for num in sorted(nums):
             # if the groups is empty, or the diff betwwen the curr and prev greater than limit
             if not groups or num - groups[-1][-1] > limit:
-                # add a new list
+                # add a new list X , use deque, as popleft will recreate indices of list, makin it O(n)
                 groups.append(deque())
             # add to the latest group in groups, the num
             groups[-1].append(num)
