a9dc55921d39d1d4cd7227fde81784e0a3cf0c41
Amudhesh <42344377+AMX0013@users.noreply.github.com>
2025-01-25 18:15:39 -0600

    Time: 247 ms (91.72%) | Memory: 95.1 MB (5.10%) - LeetSync

---
+++ 3219-make-lexicographically-smallest-array-by-swapping-elements/make-lexicographically-smallest-array-by-swapping-elements.py
@@ -0,0 +1,22 @@
+class Solution:
+    def lexicographicallySmallestArray(self, nums: List[int], limit: int) -> List[int]:
+        # put numbers post sorting into groups, based on the limit
+        groups = []
+        num_group_map = defaultdict(int)
+
+        for num in sorted(nums):
+            # if the groups is empty, or the diff betwwen the curr and prev greater than limit
+            if not groups or num - groups[-1][-1] > limit:
+                # add a new list
+                groups.append(deque())
+            # add to the latest group in groups, the num
+            groups[-1].append(num)
+            num_group_map[num] = len(groups)-1
+        
+        for idx,num in enumerate(nums):
+            grpId = num_group_map[num]
+            nums[idx] = groups[grpId].popleft()
+
+        return nums
+
+        
