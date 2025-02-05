7aa9db2c9a49a4fe4c50d64d7670308658c1877c
Amudhesh <42344377+AMX0013@users.noreply.github.com>
2025-02-05 01:37:58 -0600

    Time: 5 ms (27.89%) | Memory: 17.9 MB (57.28%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.py
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.py
@@ -1,25 +1,17 @@
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
-        costMemo = defaultdict(int) 
-        def recur(step: int):
-            # caching
-            if step in costMemo.keys():
-                return costMemo[step]
-            # stopp condition1 : notice how its len(cost),
-            #  the index that 1 larger that the last visitable index in a zero indexed array
-            if step == len(cost) :
-                return 0
-            # stopp condition2
-            if step > len(cost):
-                return 10**9
-            # init
-            if step == -1:
-                costMemo[step] = min(recur(step+1), recur(step+2))
-            else:
-                costMemo[step] = cost[step]+ min(recur(step+1), recur(step+2))
-
-            return costMemo[step]
-            
-        # call 
-        recur(-1)
-        return min(costMemo[0], costMemo[1])
+        # 1D dp
+        # funny words, u must reach idx len, while last idx of arr is len-1
+        dp = deque()
+        
+        dp.append(cost[-1])
+        dp.append(0)
+        print(dp)
+        for i in range(len(cost)-2,-1,-1):
+            # len shall always be 2 only
+            # no heap bs needed so far
+            pay = cost[i] + min(dp)
+            dp.pop()
+            dp.appendleft(pay)
+            # print('post',i,'need to pay', pay, 'dp:',dp)
+        return min(dp)
